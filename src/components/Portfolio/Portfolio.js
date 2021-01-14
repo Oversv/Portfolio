c0201e2aed8822e7992e340e173ca888977c4f8a
oversv <albertomdv@gmail.com>
2021-01-14 17:23:38 +0000

    portfolio item changed

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,21 +6,22 @@ import './Portfolio.scss'
 const Portfolio = () => {
     return (
         <section id="portfolio" className="portfolio">
-            <h3>Portfolio</h3>    
-
+            <h3>Portfolio</h3>
             {
-                dataJson.map(projects =>{
+                dataJson.map(projects =>{           
                     return( 
                         <PortfolioItem  
                             key={projects.id}
                             name={projects.name}
                             descrption={projects.description}
+                            img={projects.img}
+                            live={projects['url-live']}
+                            code={projects['url-code']}
+                            technologies={projects.technologies}
                         />
                     )
-                })
-                
+                })                
             }           
-           
         </section>
     )
 }
--- src/components/Portfolio/PortfolioItem.js
+++ src/components/Portfolio/PortfolioItem.js
@@ -1,22 +1,53 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+import { v4 as id } from 'uuid';
 
-const PortfolioItem = ({name, descrption}) => {
+const PortfolioItem = ({name, descrption, img, live, code, technologies =[]}) => {
+
+    const image = `./assets/img/${img}`
+
     return (
         <article> 
             <figure>
-                <img src="" atl=""></img>
+                <img src={image} atl="name"></img>
             </figure>
-            <h4>{name}</h4>
-            <p>{descrption}</p>
             <div>
-                <a href="#" target="_blank">See code</a>
-                <a href="#" target="_blank">See website</a>
-            </div>
-            <div>
-                Tecnologías, se puede recorrer de una lista
+                <h4>{name}</h4>
+                <p>{descrption}</p>
+
+                <div>                    
+                    <p>Technologies</p>
+                    <ul>
+                        {
+                            technologies.map(tech => (                                
+                                <li key={id()}>
+                                   <img 
+                                        src={`./assets/img/tech-icons/${tech.toLowerCase()}.png`}
+                                        alt={tech}
+                                    />                            
+                                </li>
+                                //TODO Mirar como organizan las carpetas de las imagenes estaticas en react
+                                //TODO Añadir iconos para las tecnologías                                
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div>
+                    <a href={live} target="_blank">See Website</a>
+                    <a href={code} target="_blank">See Code</a>
+                </div>
             </div>
         </article>
     )
+}
+
+PortfolioItem.propTypes = {
+    name: PropTypes.string.isRequired,
+    descrption: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    live: PropTypes.string.isRequired,
+    code: PropTypes.string.isRequired,
+    technologies: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default PortfolioItem
